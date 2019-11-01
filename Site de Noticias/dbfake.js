ea110b06c20baba23f37b2768754e64b97be86a3
Shybaraia <54871088+Shybaraia@users.noreply.github.com>
2019-11-01 14:46:31 -0300

    Add files via upload

---
+++ Site de Noticias/dbfake.js
@@ -0,0 +1,17 @@
+var db = {
+    dados: [
+        {
+            titulo: 'Noticia 1',
+            descricao: 'Descriçao noticia 1'
+        }, {
+            
+            titulo: 'Noticia 2',
+            descricao: 'Descriçao noticia 2'
+        
+        }, {
+            
+            titulo: 'Noticia 3',
+            descricao: 'Descriçao noticia 3'        
+        }
+    ]
+}
